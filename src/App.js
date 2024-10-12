919e2cff86909f7c5942bb964aefc5587062c76f
Swiss Tangsatjatham <tintinguy.01@gmail.com>
2024-10-12 16:59:19 +0700

    updating our directory page with categories components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+// Import scss
+import "./categories.styles.scss";
 
-function App() {
+// Import components
+import CategoriesList from "./components/categories-list/categories-list.component";
+
+const App = () => {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CategoriesList />
   );
-}
+};
 
 export default App;
---
+++ src/components/categories-list/categories-list.component.jsx
@@ -0,0 +1,45 @@
+// Import components
+import CategoryItem from "../category-item/category-item.component";
+
+// Import scss
+import "./categories-list.styles.scss";
+
+const CategoriesList = () => {
+    const categories = [
+        {
+          "id": 1,
+          "title": "Hats",
+          "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+        },
+        {
+          "id": 2,
+          "title": "Jackets",
+          "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+        },
+        {
+          "id": 3,
+          "title": "Sneakers",
+          "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+        },
+        {
+          "id": 4,
+          "title": "Womens",
+          "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+        },
+        {
+          "id": 5,
+          "title": "Mens",
+          "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+        }
+      ];
+
+      return (
+        <div className="categories-list-container">
+        {categories.map((category) => (
+            <CategoryItem key={category.id} category={category}  />
+        ))}
+        </div>
+      );
+};
+
+export default CategoriesList;
